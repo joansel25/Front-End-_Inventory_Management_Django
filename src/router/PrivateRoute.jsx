133daf3b9d04f0ba87367744149a5dfa41499121
joansel25 <joan.cardenas@correo.tdea.edu.co>
2025-11-13 11:56:02 -0500

    rama principal

--- src/router/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-function PrivateRoute({ children, rolPermitido }) {
-  const rol = localStorage.getItem("rol");
-  return rol === rolPermitido ? children : <Navigate to="/" />;
-}
-
-export default PrivateRoute;
